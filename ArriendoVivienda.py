aa61984c64dd33144d8262b9fae8be4dcfbab362
JavDB3 <jav.duranb@duocuc.cl>
2024-05-30 09:51:31 -0400

    Update ArriendoVivienda.py

--- ArriendoVivienda.py
+++ ArriendoVivienda.py
@@ -1,15 +1,1 @@
-num1 = input("Ingresa el primer número: ")
-num2 = input("Ingresa el segundo número: ")
 
-num1 = int(num1)
-num2 = int(num2)
-
-suma = num1 + num2
-resta = num1 - num2
-multiplicacion = num1 * num2
-division = num1 / num2 
-
-print("Suma:", suma)
-print("Resta:", resta)
-print("Multiplicación:", multiplicacion)
-print("División:", division)
