7652c9cacea99e3790816410424a98d1a9210aa2
franar22 <fran.argandona@duocuc.cl>
2024-06-06 09:14:54 -0400

    Update PrincipalArriendoVivienda.py

--- PrincipalArriendoVivienda.py
+++ PrincipalArriendoVivienda.py
@@ -6,9 +6,9 @@ while True:
   print("3.Años trabajados")
   print("4.Renta mensual ")
   print("5.Salir")
-  opcion=int(input("elige una opcion: "))
+  opcion=int(input("eliga una opcion: "))
   if opcion==1:
     print(input("Ingrese su primer nombre y sus dos apellidos: "))
   if opcion==2:
     for usuario in range ():
-      print(int(input("Ingrese  su edad (dia/mes/año): ")))
+      print(int(input("Ingrese su fecha de nacimiento (dia/mes/año): ")))
