a42001be017fa193b8208cf1157255c2361f1353
JavDB3 <jav.duranb@duocuc.cl>
2024-06-06 09:02:15 -0400

    Update PrincipalArriendoVivienda.py

--- PrincipalArriendoVivienda.py
+++ PrincipalArriendoVivienda.py
@@ -6,8 +6,8 @@ while True:
   print("3.")
   print("4.")
   print("5.Salir")
-  opcion=int(input("elige una opcion"))
+  opcion=int(input("elige una opcion: "))
   if opcion==1:
-    print(input("Ingrese su primer nombre y sus dos apellidos"))
+    print(input("Ingrese su primer nombre y sus dos apellidos: "))
   if opcion==2:
     print(int(input("Ingrese su usuario(dia/mes/año):      ")))
