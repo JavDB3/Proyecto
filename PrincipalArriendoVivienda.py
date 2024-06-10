8190bb6dbb9f280f129c29ef1ff7caff938edc98
JavDB3 <jav.duranb@duocuc.cl>
2024-06-10 08:43:00 -0400

    Update PrincipalArriendoVivienda.py

--- PrincipalArriendoVivienda.py
+++ PrincipalArriendoVivienda.py
@@ -2,19 +2,6 @@
 import Libreria as lb
 while True:
   print("1.Validar el usuario" )
-  print("2.Edad del usuario")
-  print("3.Años trabajados")
-  print("4.Renta mensual ")
-  print("5.Salir")
-  opcion=int(input("eliga una opcion: "))
-  if opcion==1:
-    print(input("Ingrese su primer nombre y sus dos apellidos: "))
-  if opcion==2:
-    for usuario in range ():
-      print(int(input("Ingrese su fecha de nacimiento (dia/mes/año): ")))
-  if opcion==3:
-    print(int(input("Ingrese cuantos años a trabajado:")))
-     if opcion==3 =0:
-       print("No puede arrendar debido a que tiene menos de un año trabajado")
+
        
  
